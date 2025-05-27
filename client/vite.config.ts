70e5f8862e574cef268364e91c34e58c16f1e49c
suuumii <76721789+suuumii@users.noreply.github.com>
2025-05-27 23:59:34 +0900

    create client pjt

---
+++ client/vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), crx({ manifest })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    cors: {
+      origin: [
+        // ⚠️ SECURITY RISK: Allows any chrome-extension to access the vite server ⚠️
+        // See https://github.com/crxjs/chrome-extension-tools/issues/971 for more info
+        // I don't believe that the linked issue mentions a potential solution
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
+  legacy: {
+    // ⚠️ SECURITY RISK: Allows WebSockets to connect to the vite server without a token check ⚠️
+    // See https://github.com/crxjs/chrome-extension-tools/issues/971 for more info
+    // The linked issue gives a potential fix that @crxjs/vite-plugin could implement
+    skipWebSocketTokenCheck: true,
+  },
+})
